78d9850ab49be4b49587f71fab2b33ca524ba464
rakmans <rakmans.team@gmail.com>
2024-04-23 17:52:13 +0330

    update appbar and footer

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,12 +1,12 @@
 import {
-  CssBaseline,
-  Fab,
-  Container,
-  AppBar,
-  Box,
-  Toolbar,
-  useTheme,
-  Typography,
+    CssBaseline,
+    Fab,
+    Container,
+    AppBar,
+    Box,
+    Toolbar,
+    useTheme,
+    Typography,
 } from "@mui/material";
 import { UilAngleUp } from "@iconscout/react-unicons";
 
@@ -15,61 +15,71 @@ import { Mode, rakmans } from "./index";
 import WalletConnect from "../../components/WalletConnectButton";
 
 const Appbar = () => {
-  const theme = useTheme();
+    const theme = useTheme();
 
-  return (
-    <>
-      <CssBaseline />
-      <ElevationScroll>
-        <AppBar
-          className="appBar"
-          sx={{
-            width: "100%",
-            p: 0.5,
-            backdropFilter: "blur(3px)",
-            borderBottom:
-              theme.palette.mode === "dark" ? "3px solid #40679E" : "",
-            WebkitBackdropFilter: "blur(3px)",
-            zIndex: 9,
-          }}
-        >
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-              <Box
-                sx={{
-                  mr: 2,
-                  display: "flex",
-                  flexGrow: 1,
-                  fontSize: "30px",
-                }}
-              >
-                <Box component="img" src={rakmans} alt="logo" width={50} />
-                <Typography
-                  variant="h4"
-                  mt={0.9}
-                  ml={1}
-                  display={{ md: "block", xs: "none" }}
-                >
-                  Rakmans
-                </Typography>
-              </Box>
-
-              <Box sx={{ flexGrow: { xs: 1, sm: 0 } }} display={"flex"}>
-                <WalletConnect />
-              </Box>
-              <Mode />
-            </Toolbar>
-          </Container>
-        </AppBar>
-      </ElevationScroll>
-      <Box id="back-to-top-anchor" />
-      <ScrollTop>
-        <Fab color="primary" size="small">
-          <UilAngleUp />
-        </Fab>
-      </ScrollTop>
-    </>
-  );
+    return (
+        <>
+            <CssBaseline />
+            <ElevationScroll>
+                <AppBar
+                    className='appBar'
+                    sx={{
+                        width: "100%",
+                        p: 0.5,
+                        backdropFilter: "blur(3px)",
+                        borderBottom:
+                            theme.palette.mode === "dark"
+                                ? "3px solid #40679E"
+                                : "",
+                        WebkitBackdropFilter: "blur(3px)",
+                        zIndex: 9,
+                    }}>
+                    <Container maxWidth='xl'>
+                        <Toolbar disableGutters>
+                            <Box
+                                component='a'
+                                target='_blank'
+                                href='https://rakmans.github.io/'
+                                sx={{
+                                    mr: 2,
+                                    display: "flex",
+                                    flexGrow: 1,
+                                    fontSize: "30px",
+                                    textDecoration:"none",
+                                    color:"white"
+                                }}>
+                                <Box
+                                    component='img'
+                                    src={rakmans}
+                                    alt='logo'
+                                    width={50}
+                                />
+                                <Typography
+                                    variant='h4'
+                                    mt={0.9}
+                                    ml={1}
+                                    display={{ md: "block", xs: "none" }}>
+                                    Rakmans
+                                </Typography>
+                            </Box>
+                            <Box
+                                sx={{ flexGrow: { xs: 1, sm: 0 } }}
+                                display={"flex"}>
+                                <WalletConnect />
+                            </Box>
+                            <Mode />
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+            </ElevationScroll>
+            <Box id='back-to-top-anchor' />
+            <ScrollTop>
+                <Fab color='primary' size='small'>
+                    <UilAngleUp />
+                </Fab>
+            </ScrollTop>
+        </>
+    );
 };
 
 export default Appbar;
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,82 +1,115 @@
 import { Box, IconButton, Typography, useTheme } from "@mui/material";
 import {
-  UilLinkedin,
-  UilInstagram,
-  UilTelegram,
-  UilWallet,
+    UilLinkedin,
+    UilInstagram,
+    UilTelegram,
+    UilWallet,
 } from "@iconscout/react-unicons";
 import rakmans from "./rakmans.svg";
+import { copyToClipboard } from "../../utils";
 
 const Footer = () => {
-  const theme = useTheme();
-  return (
-    <>
-      <Box
-        sx={{
-          m: "auto",
-          mt: "1%",
-        }}
-      >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            pt: "4vh",
-          }}
-        >
-          <Box
-            component="img"
-            src={rakmans}
-            sx={{
-              m: "auto",
-              width: 80,
-            }}
-          />
-        </Box>
-
-        <Box sx={{ textAlign: "center", mt: "1vh", pb: "2vh" }}>
-          <IconButton sx={{ mr: "2.5vw" }}>
-            <UilLinkedin
-              color={theme.palette.mode === "dark" ? "#faf69c" : "#FFD700"}
-              size="3vw"
-            />
-          </IconButton>
-          <IconButton sx={{ mr: "1.25vw" }}>
-            <UilInstagram
-              color={theme.palette.mode === "dark" ? "#faf69c" : "#FFD700"}
-              size="3vw"
-            />
-          </IconButton>
-          <Typography
-            variant="p"
-            color={theme.palette.mode === "dark" ? "#90caf9" : "#0d47a1"}
-            sx={{
-              m: "auto",
-              mt: "2vh",
-
-              fontSize: "2vw",
-              fontWeight: "700",
-            }}
-          >
-            RAKMANS
-          </Typography>
-          <IconButton sx={{ ml: "1.25vw" }}>
-            <UilTelegram
-              size="3vw"
-              color={theme.palette.mode === "dark" ? "#faf69c" : "#FFD700"}
-            />
-          </IconButton>
-
-          <IconButton sx={{ ml: "2.5vw" }}>
-            <UilWallet
-              color={theme.palette.mode === "dark" ? "#faf69c" : "#FFD700"}
-              size="3vw"
-            />
-          </IconButton>
-        </Box>
-      </Box>
-    </>
-  );
+    const theme = useTheme();
+    return (
+        <>
+            <Box
+                sx={{
+                    m: "auto",
+                    mt: "1%",
+                }}>
+                <Box
+                    component='a'
+                    target='_blank'
+                    href='https://rakmans.github.io/'
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        pt: "4vh",
+                    }}>
+                    <Box
+                        component='img'
+                        src={rakmans}
+                        sx={{
+                            m: "auto",
+                            width: 80,
+                        }}
+                    />
+                </Box>
+                <Box sx={{ textAlign: "center", mt: "1vh", pb: "2vh" }}>
+                    <IconButton
+                        sx={{ mr: "2.5vw" }}
+                        target='_blank'
+                        href='https://www.linkedin.com/in/rakmans/'>
+                        <UilLinkedin
+                            color={
+                                theme.palette.mode === "dark"
+                                    ? "#faf69c"
+                                    : "#FFD700"
+                            }
+                            size='3vw'
+                        />
+                    </IconButton>
+                    <IconButton sx={{ mr: "1.25vw" }}>
+                        <UilInstagram
+                            color={
+                                theme.palette.mode === "dark"
+                                    ? "#faf69c"
+                                    : "#FFD700"
+                            }
+                            size='3vw'
+                        />
+                    </IconButton>
+                    <Typography
+                        component='a'
+                        target='_blank'
+                        href='https://rakmans.github.io/'
+                        color={
+                            theme.palette.mode === "dark"
+                                ? "#90caf9"
+                                : "#0d47a1"
+                        }
+                        sx={{
+                            m: "auto",
+                            mt: "2vh",
+                            fontSize: "2vw",
+                            fontWeight: "700",
+                            textDecoration: "none",
+                        }}>
+                        RAKMANS
+                    </Typography>
+                    <IconButton
+                        sx={{ ml: "1.25vw" }}
+                        target='_blank'
+                        href='https://t.me/rakmans_support'>
+                        <UilTelegram
+                            size='3vw'
+                            color={
+                                theme.palette.mode === "dark"
+                                    ? "#faf69c"
+                                    : "#FFD700"
+                            }
+                        />
+                    </IconButton>
+                    <IconButton
+                        sx={{ ml: "2.5vw" }}
+                        onClick={() =>
+                            copyToClipboard(
+                                "0x8dedDf9068B594310b8914079CA41CE1cb5Bf6D0"
+                            )
+                        }>
+                        <UilWallet
+                            color={
+                                theme.palette.mode === "dark"
+                                    ? "#faf69c"
+                                    : "#FFD700"
+                            }
+                            size='3vw'
+                        />
+                    </IconButton>
+                </Box>
+            </Box>
+        </>
+    );
 };
 
 export default Footer;
